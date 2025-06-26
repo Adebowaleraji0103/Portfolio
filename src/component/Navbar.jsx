9a2ccc42145634dae889de2c624d68ead8dbf983
Mubarak <adebowaleraji0103@gmail.com>
2025-06-26 22:25:23 +0100

    added some new feature

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center px-[2rem] py-[7px] shadow-gray-250 shadow-lg">
+    <div className="flex justify-between items-center px-[2rem] py-[7px] shadow-gray-250 shadow-lg sticky scroll-smooth">
       <h2 className="font-bold text-[#2ecc71] bg-[#f5f5f5] px-5 py-2 w-fit rounded-2xl">
         {" "}
         Debo
