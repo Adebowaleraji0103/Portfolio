5eadf318c596de7822977cccae730b6f4b0be753
Mubarak <adebowaleraji0103@gmail.com>
2025-06-26 16:17:28 +0100

    added

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,9 +1,9 @@
 const About = () => {
   return (
-    <div className="section flex flex-col gap-5 justify-center items-center text-center w-[100%] px-[10px]">
+    <div className="section flex flex-col gap-5 justify-center items-center text-center w-[100%] px-[10px] py-[20px]">
       <h1 className="font-bold text-[20px] ">About Me</h1>
 
-        <div className="w-full  border-3 border-[#2ecc71] object-cover ">
+        <div className="w-full  border-4 border-[#2ecc71] object-cover ">
           <img className="w-full h-full" src="./images/Learn JavaScript skills from scratch for only $30.jpg" alt="" />
         </div>
 
