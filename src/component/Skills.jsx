58af4a175989a67af5ce3088e7ba31912292e51c
Mubarak <adebowaleraji0103@gmail.com>
2025-07-02 21:42:49 +0100

    s

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,12 +1,12 @@
 const skills = () => {
   return (
     <div
-      className="section py-[40px] px-[10px] pb-[20px] flex flex-col justify-center items-center text-center"
+      className="section py-[40px] px-[10px] pb-[20px] flex flex-col gap-10 shadowSection justify-center items-center text-center"
       id="skills"
     >
       <h1 className="text-center font-bold text-[30px] pb-4">Skills</h1>
 
-      <div className="w-fit text-[18px] font-bold pb-[35px] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-5 gap-x-9 justify-center items-center text-center">
+      <div className="secton  w-fit text-[18px] font-bold pb-[35px] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-5 gap-x-9 justify-center items-center text-center">
         <div className="skillSection ">
           <h2>HTML</h2>
         </div>
