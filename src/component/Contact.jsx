17fb7b37303b44839307cb3a80017bddc60ce94c
Mubarak <adebowaleraji0103@gmail.com>
2025-07-02 22:00:13 +0100

    kjsf

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,12 +1,15 @@
 const Contact = () => {
   return (
-    <div className="shadowSection mb-5 section " id="contact">
+    <div
+      className="flex flex-col items-center m-auto gap-10 w-[90%] pt-[20px] mt-[20px] shadow-2xl shadow-gray-400 lg:shadow px-[10px]  mb-5  "
+      id="contact"
+    >
       <h1>Get In Touch</h1>
 
-      <form action="">
+      <form action="w-full">
         <div>
           <label htmlFor="">Name</label>
-          <input type="text" />
+          <input type="text" className="border w-[90%]" />
         </div>
 
         <div>
@@ -15,7 +18,7 @@ const Contact = () => {
         </div>
 
         <div>
-            <textarea name="" id=""></textarea>
+          <textarea name="" id=""></textarea>
         </div>
       </form>
     </div>
