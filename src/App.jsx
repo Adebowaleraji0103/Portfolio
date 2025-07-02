0d04f490d0f8853040a3dab182158c4095710ee1
Mubarak <adebowaleraji0103@gmail.com>
2025-07-02 13:08:13 +0100

    jst some features in contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import About from "./component/About";
 import Skills from "./component/Skills";
+import Contact from "./component/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Skills/>
+      <Contact/>
     </div>
   );
 }
--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,10 +1,25 @@
 const Contact = () => {
-    return ( 
+  return (
+    <div>
+      <h1>Get In Touch</h1>
+
+      <form action="">
         <div>
-            <h1>Contact</h1>
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" />
+        </div>
+
+        <div>
             
         </div>
-     );
-}
- 
+      </form>
+    </div>
+  );
+};
+
 export default Contact;
