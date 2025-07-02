19209a9b918913086ac8d18ac9ce4e8f8a55d567
Mubarak <adebowaleraji0103@gmail.com>
2025-07-02 14:39:47 +0100

    ndmd

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,11 +1,14 @@
+import { FaHtml5 } from "react-icons/fa6";
+
 
 const skills = () => {
   return (
-    <div className="section py-[40px] px-[10px] flex flex-col justify-center items-center text-center" id="skills">
+    <div className="section py-[40px] px-[10px] pb-[20px] flex flex-col justify-center items-center text-center" id="skills">
       <h1 className="text-center font-bold text-[30px] pb-4">Skills</h1>
 
-      <div className="w-fit grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-5 gap-x-14 justify-center items-center text-center">
+      <div className="w-fit pb-[35px] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-5 gap-x-14 justify-center items-center text-center">
         <div className="w-[7rem] bg-[#2ecc71] text-white font-semibold py-2 px-5 rounded-[10px]">
+          <FaHtml5 />
           <h2>HTML</h2>
         </div>
 
