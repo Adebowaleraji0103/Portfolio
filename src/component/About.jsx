5eb59499ba300069a7745b76bf65ac8a8a34e6dc
Mubarak <adebowaleraji0103@gmail.com>
2025-07-02 19:50:14 +0100

    sj

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,23 +1,29 @@
 const About = () => {
   return (
-    <div className="flex flex-col gap-10 bg-white shadow-lg shadow-gray-300  justify-center items-center text-center w-[100%] px-[10px] py-[20px] " id="about">
+    <div
+      className="flex flex-col gap-10 bg-white shadow-lg shadow-gray-300  justify-center items-center text-center w-[100%] px-[10px] py-[20px] "
+      id="about"
+    >
       <h1 className="font-bold text-[30px]  ">About Me</h1>
 
+      <div className="flex px-8 grid-cols-1 sm:grid-cols-2 gap-5 items-center ">
 
-      <div className=" grid grid-cols-1 sm:grid-cols-2 gap-5 items-center ">
-      <div className="w-full  border-7 border-[#2ecc71] bor object-cover ">
-        <img
-          className="w-full h-full"
-          src="./images/Learn JavaScript skills from scratch for only $30.jpg"
-          alt=""
-        />
-      </div>
+        <div className="w-full border-7 border-[#2ecc71] object-cover ">
+          <img
+            className="w-full h-full "
+            src="./images/mubarak.jpeg"
+            alt="helo"
+          />
+        </div>
 
         <p className="w-full ">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
           vitae deserunt est hic ratione reiciendis blanditiis rem aut harum
           explicabo quisquam iusto, saepe esse repellat. Iusto quaerat molestias
-          magnam corrupti!
+          magnam corrupti!Lorem Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Ab est consequatur minus quos perspiciatis soluta
+          totam quaerat, quasi neque velit debitis sint animi fuga quis.
+          Reprehenderit eius commodi alias quia!
         </p>
       </div>
     </div>
