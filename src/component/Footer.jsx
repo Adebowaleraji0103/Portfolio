9f59dc9fad967e674b940a6f9af36668119c1574
Mubarak <adebowaleraji0103@gmail.com>
2025-07-12 15:21:42 +0100

    s

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <div className="bg-[#2c3e50] w-full h-[14rem] mt-10">
+        <div className="bg-[#2c3e50] w-full h-[14rem] mt-[20px] pt-[20px]">
             <h1>Footer</h1>
         </div>
      );
