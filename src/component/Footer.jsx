815d2bc0561520ac54d0bf9296ef708ca68dd07b
Mubarak <adebowaleraji0103@gmail.com>
2025-07-12 16:01:50 +0100

    s

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,6 +3,11 @@ const Footer = () => {
     <footer className="bg-[#2c3e50] w-full h-[14rem] mt-[20px] pt-[20px]">
       <h2>© 2025 Adebowale | Web Designer</h2>
       
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li></li>
+        <li></li>
+      </ul>
 
     </footer>
   );
