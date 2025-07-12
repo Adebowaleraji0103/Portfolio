4edb66ad923b8adb98bc0407b024ea220f76b0d0
Mubarak <adebowaleraji0103@gmail.com>
2025-07-12 16:04:30 +0100

    added the footer link

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -7,6 +7,9 @@ const Footer = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#project">Project</a></li>
         <li><a href="#skills">Skills</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#certificate">Certificate</a></li>
+        <li><a href="contact">Contact</a></li>
       </ul>
 
     </footer>
