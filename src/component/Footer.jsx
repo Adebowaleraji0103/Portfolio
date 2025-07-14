ffa1b70bdddd8ba2358bdaa8f0376abd41fa5184
Mubarak <adebowaleraji0103@gmail.com>
2025-07-14 23:48:50 +0100

    s

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -13,8 +13,7 @@ const Footer = () => {
         {/* <a href="https://web.whatsapp.com/"><li><FaWhatsapp /></li></a> */}
         <a href="https://wa.me/<+2349156522875>" target="blank"><FaWhatsapp /></a>
         <li><a href="https://github.com/Adebowaleraji0103/"><FaGithub /> </a></li>
-        <a href="https://mailto:mubarakadebowale1@gmail.com"><li><SiGmail /></li></a>
-       <a href="https://linkedin.com/in/raji-mubarak-04a93726a"> <li><FaLinkedin /></li></a>
+        <a href="https://mailto:mubarakadebowale1@gmail.com"><li><SiGmail /></li></a       <a href="https://linkedin.com/in/raji-mubarak-04a93726a"> <li><FaLinkedin /></li></a>
       </ul>
 
     </footer>
