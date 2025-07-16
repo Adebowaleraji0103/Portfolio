bfb7e93b74d3c81f97a6ad65c6eda3dcfb6416e5
Mubarak <adebowaleraji0103@gmail.com>
2025-07-17 00:59:51 +0100

    s

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,12 +1,15 @@
 const Landing = () => {
   return (
-    <div className="bg-[#2ecc71] w-full h-[100vh]">
-      <div className="flex justify-center my-auto items-center  w-[350px] h-[350px] bg-amber-50 rounded-full "></div>
-      <h1>
-        Hi, I’m Adebowale.
-      </h1>
-      <button className="py-7 px-15 bg-amber-600 font-bold">welcome</button>
-      <link rel="stylesheet" href="" />
+    <div className="flex flex-col gap-9 items-center bg-[#4dd44d] h-[100vh]">
+      <div className=" w-[350px] h-[350px] bg-amber-50 rounded-full ">  </div>
+
+      <h2>Hello I'm Adebowale</h2>
+
+      <div className="flex gap-3">
+        <button className="py-2 px-4 bg-amber-50 text-black font-semibold rounded-[10px]">View Profile</button>
+         <button className="py-2 px-4 bg-amber-50 text-black font-semibold rounded-[10px]">Download Cv</button>
+      </div>
+      
     </div>
   );
 };
