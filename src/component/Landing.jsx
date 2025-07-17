6ef9f6e27e7da34350591266a028d805ca449b65
Mubarak <adebowaleraji0103@gmail.com>
2025-07-17 16:12:43 +0100

    d

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -2,11 +2,17 @@ const Landing = () => {
   return (
     <div>
       <header className=" shadow-gray-250 shadow-lg bg-transparent w-full px-[2rem] py-[7px] scroll-smooth mb-5">
-          <h2 className="font-bold text-[#2ecc71] bg-[gray] px-5 py-2 w-fit rounded-2xl">Debo </h2>
+        <h2 className="font-bold text-[#2ecc71] bg-[gray] px-5 py-2 w-fit rounded-2xl">
+          Debo{" "}
+        </h2>
       </header>
 
       <div className="flex flex-col gap-9 items-center  h-[100vh] ">
-        <div className=" w-[350px] h-[350px] bg-[#4dd44d] rounded-full "> </div>
+        <div className=" w-[350px] h-[350px] bg-[#4dd44d] rounded-full overflow-hidden">
+          <div className="p-5 rounded-full">
+            <img src="./images/my profile pics.jpeg" alt="" className="w-full h-full rounded-full"/>
+          </div>
+        </div>
 
         <h2>Hello I'm Adebowale</h2>
 
