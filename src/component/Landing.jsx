97c3f20fcb011d5ecace812630d46e1f3ee4003a
Mubarak <adebowaleraji0103@gmail.com>
2025-07-17 15:26:02 +0100

    d

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,8 +1,12 @@
 const Landing = () => {
   return (
     <div>
-      <header>
-        <h1>Debo</h1>
+      <header className="fixed flex flex-col md:flex-row md:justify-between md:items-center top-0 right-0 left-0 shadow-gray-250 shadow-lg bg-white">
+        <nav className="px-[2rem] py-[7px] scroll-smooth">
+          <h2 className="font-bold text-[#2ecc71] bg-[gray] px-5 py-2 w-fit rounded-2xl">
+            Debo
+          </h2>
+        </nav>
       </header>
       <div className="flex flex-col gap-9 items-center bg-[#4dd44d] h-[100vh]">
         <div className=" w-[350px] h-[350px] bg-amber-50 rounded-full "> </div>
