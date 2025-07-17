fef428303cbccc180f68590e0f5fbb42304377ac
Mubarak <adebowaleraji0103@gmail.com>
2025-07-17 15:22:03 +0100

    d

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,15 +1,23 @@
 const Landing = () => {
   return (
-    <div className="flex flex-col gap-9 items-center bg-[#4dd44d] h-[100vh]">
-      <div className=" w-[350px] h-[350px] bg-amber-50 rounded-full ">  </div>
+    <div>
+      <header>
+        <h1>Debo</h1>
+      </header>
+      <div className="flex flex-col gap-9 items-center bg-[#4dd44d] h-[100vh]">
+        <div className=" w-[350px] h-[350px] bg-amber-50 rounded-full "> </div>
 
-      <h2>Hello I'm Adebowale</h2>
+        <h2>Hello I'm Adebowale</h2>
 
-      <div className="flex gap-3">
-        <button className="py-2 px-4 bg-amber-50 text-black font-semibold rounded-[10px]">View Profile</button>
-         <button className="py-2 px-4 bg-amber-50 text-black font-semibold rounded-[10px]">Download Cv</button>
+        <div className="flex gap-3">
+          <button className="py-2 px-4 bg-amber-50 text-black font-semibold rounded-[10px]">
+            View Profile
+          </button>
+          <button className="py-2 px-4 bg-amber-50 text-black font-semibold rounded-[10px]">
+            Download Cv
+          </button>
+        </div>
       </div>
-      
     </div>
   );
 };
