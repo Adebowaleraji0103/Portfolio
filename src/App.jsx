46601a677ebba748f44c519ed770162904b64aef
Mubarak <adebowaleraji0103@gmail.com>
2025-07-18 15:57:08 +0100

    djof

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./component/Landing";
 import Profilepage from "./component/Profilepage/ProfilePage";
 
 function App() {
   return (
     <div>
-      <Landing />
-      {/* <Profilepage /> */}
+      <Router>
+        <Landing />
+        <Profilepage />
+      </Router>
     </div>
   );
 }
